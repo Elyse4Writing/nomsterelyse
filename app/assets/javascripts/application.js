f42f339d4bc0cd58dabc0ac5518175c6a19a14cc
elyse.glickman <ejglickmanmedia@gmail.com>
2017-10-04 13:13:15 +0000

    revisedbootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,9 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
 
