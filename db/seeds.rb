ec05f63bbbdb46ee248b3c5ac455146189b93786
elyse.glickman <ejglickmanmedia@gmail.com>
2017-10-07 00:59:49 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'Cactus Taqueria', address: '4368 Woodman Avenue, Sherman Oaks, CA', pricerange: 'inexpensive', description: 'The nachos are the highlight at this classic local taco joint. Be sure to try the new shrimp tacos, too!
+')
+Place.create(name: 'Taste Chicago', address: '630 North Hollywood Way, Burbank, CA
+', pricerange: 'inexpensive to moderate',  description: 'Chicago deep dish pizza, Vienna hot dogs, Italian beef, and more. New desserts from Eli\'s Cheesecake coming soon!')
+Place.create(name: 'Big Fish', address: '4360 Woodman Avenue, Sherman Oaks, CA', pricerange: 'inexpensive',  description: 'Good quality sushi and noodles, and nice lunch specials.')
